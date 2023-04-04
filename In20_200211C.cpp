d4ae5e7dec5f4e05345cefa172c50014a2553cd4
Henoshan <107171366+henoshan@users.noreply.github.com>
2023-04-04 18:10:20 +0530

    Update In20_200211C.cpp

--- In20_200211C.cpp
+++ In20_200211C.cpp
@@ -91,3 +91,111 @@ int main() {
     
     return 0;
 }
+
+
+
+
+// Using Linked list
+struct Node {
+    int data; 
+    Node* next; 
+};
+
+
+class Stack {
+private:
+    Node* top; 
+public:
+    Stack() {
+        top = NULL; 
+    }
+
+    
+    void push(int val) {
+        Node* newNode = new Node; 
+        newNode->data = val; 
+        newNode->next = top; 
+        top = newNode; 
+    }
+
+    
+    void pop() {
+        if (top == NULL) { 
+            cout << "Stack is empty" << endl;
+            return;
+        }
+        Node* temp = top; 
+        top = top->next; 
+        delete temp; 
+    }
+
+    
+    bool isEmpty() {
+        return top == NULL; 
+    }
+
+    
+    bool isFull() {
+        return false; 
+    }
+
+    
+    int stackTop() {
+        if (top == NULL) { 
+            cout << "Stack is empty" << endl;
+            return -1;
+        }
+        return top->data; 
+    }
+
+    
+    void display() {
+        if (top == NULL) { 
+            cout << "Stack is empty" << endl;
+            return;
+        }
+        Node* temp = top; 
+        while (temp != NULL) { 
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+// Example usage
+
+
+
+
+int main() {
+    Stack s;
+
+    auto start = chrono::high_resolution_clock::now(); // start the timer
+    s.push(10);
+    s.push(5);
+    s.push(11);
+    s.push(15);
+    s.push(23);
+    s.push(6);
+    s.push(18);
+    s.push(20);
+    s.push(17);
+    s.display();
+    s.pop();
+    s.pop();
+    s.pop();
+    s.pop();
+    s.pop();
+    s.display();
+    s.push(4);
+    s.push(30);
+    s.push(3);
+    s.push(1);
+    s.display();
+    auto end = chrono::high_resolution_clock::now(); // end the timer
+    auto duration = chrono::duration_cast<chrono::microseconds>(end - start); // calculate duration in microseconds
+    cout << "Time taken for specific push operations using linked list: " << duration.count() << " microseconds" << endl;
+    
+    return 0;
+}
