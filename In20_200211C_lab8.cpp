954acc6e66212a4b120f21006b6f9ab26e04afc4
Henoshan <107171366+henoshan@users.noreply.github.com>
2023-05-02 17:05:36 +0530

    Update In20_200211C_lab8.cpp

--- In20_200211C_lab8.cpp
+++ In20_200211C_lab8.cpp
@@ -15,63 +15,67 @@ void heapify(int arr[], int n, int root) {
   }
  
     // If right child is larger than largest so far
-    if (right < n && arr[right] > arr[largest])
-        largest = right;
+  if (right < n && arr[right] > arr[largest]){
+    largest = right;
+  }
+        
  
-    // If largest is not root
-    if (largest != root) {
-        swap(arr[root], arr[largest]); // Swap the root with the largest child
+  // If largest is not root
+  if (largest != root) {
+    swap(arr[root], arr[largest]); // Swap the root with the largest child
  
-        // Recursively heapify the affected sub-tree
-        heapify(arr, n, largest);
-    }
+    // Recursively heapify the affected sub-tree
+    heapify(arr, n, largest);
+  }
 }
   
 // function to perform heap sort
 void heapSort(int arr[], int n) {
-    // Build heap
-    // Starting from the last non-leaf node and heapifying all subtrees
-    for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(arr, n, i);
+  // Build heap
+  // Starting from the last non-leaf node and heapifying all subtrees
+  for (int i = n / 2 - 1; i >= 0; i--){
+    heapify(arr, n, i);
+  }
  
-    // Extract elements from heap one by one
-    // Starting from the last element and placing it at the end of the array
-    // Then heapifying the reduced heap
-    for (int i=n-1; i>=0; i--) {
-        // Move current root to end
-        swap(arr[0], arr[i]);
+  // Extract elements from heap one by one
+  // Starting from the last element and placing it at the end of the array
+  // Then heapifying the reduced heap
+  for (int i=n-1; i>=0; i--) {
+    // Move current root to end
+    swap(arr[0], arr[i]);
  
-        // call max heapify on the reduced heap
-        heapify(arr, i, 0);
-    }
+    // call max heapify on the reduced heap
+    heapify(arr, i, 0);
+  }
 }
 
 // function to display the contents of an array
 void displayArray(int arr[], int n) {
-    for (int i=0; i<n; ++i)
-        cout << arr[i] << " ";
+  for (int i=0; i<n; ++i){
+    cout << arr[i] << " ";
     cout << "\n";
+  }
 }
 
 // main program
 int main() {
-    int n;
-    cout<<"Enter the number of elements: ";
-    cin>>n;
+  int n;
+  cout<<"Enter the number of elements: ";
+  cin>>n;
 
-    int heap_arr[n];
-    cout<<"Enter "<<n<<" elements: ";
-    for(int i=0;i<n;i++){
-        cin>>heap_arr[i];
-    }
+  int heap_arr[n];
+  cout<<"Enter "<<n<<" elements: ";
+  for(int i=0;i<n;i++){
+    cin>>heap_arr[i];
+  }
     
-    cout<<"Input array"<<endl;
-    displayArray(heap_arr, n);
+  cout<<"Input array"<<endl;
+  displayArray(heap_arr, n);
   
-    heapSort(heap_arr, n);
+  heapSort(heap_arr, n);
   
-    cout << "Sorted array"<<endl;
-    displayArray(heap_arr, n);
+  cout << "Sorted array"<<endl;
+  displayArray(heap_arr, n);
   
-    return 0;
+  return 0;
 }
